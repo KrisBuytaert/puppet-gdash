fc126dcdeee6ec6fe1d1b60519022051efeeab7b
Julien Pivotto <roidelapluie@inuits.eu>
2012-09-13 16:07:06 +0200

    fix indetation

--- manifests/init.pp
+++ manifests/init.pp
@@ -15,24 +15,24 @@ class gdash (
   $graphitehost='127.0.0.1'
 )
 {
- $gdashroot = '/usr/local/gdash/'
- package {"rubygem-sinatra": ensure => '1.3.2-1'}
- package {"rubygem-bundler": ensure => present }
- package {"rubygem-tilt":    ensure => present }
- package {"rubygem-rack":    ensure => present }
- package {"rubygem-rack-protection": ensure => present }
+  $gdashroot = '/usr/local/gdash/'
+  package {"rubygem-sinatra": ensure => '1.3.2-1'}
+  package {"rubygem-bundler": ensure => present }
+  package {"rubygem-tilt":    ensure => present }
+  package {"rubygem-rack":    ensure => present }
+  package {"rubygem-rack-protection": ensure => present }
 
 
 
 
- package {"gdash": ensure => present }
+  package {"gdash": ensure => present }
 
- file {
-   "${gdashroot}/config/gdash.yaml":
-     content => template('gdash/gdash.yaml.erb'),
-     group   => '0',
-     owner   => '0';
- }
+  file {
+    "${gdashroot}/config/gdash.yaml":
+      content => template('gdash/gdash.yaml.erb'),
+      group   => '0',
+      owner   => '0';
+  }
 
 
 }
