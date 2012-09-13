4d57609e25cee28bddaf418e34a06571b6ad4a42
Julien Pivotto <roidelapluie@inuits.eu>
2012-09-13 16:07:27 +0200

    use autoload layout

--- manifests/init.pp
+++ manifests/init.pp
@@ -37,15 +37,3 @@ class gdash (
 
 }
 
-class gdash::vhost {
-
-  file {
-    "/etc/httpd/conf.d/gdash.conf":
-      ensure  => 'file',
-      group   => '0',
-      mode    => '0644',
-      owner   => '0',
-      source => 'puppet:///modules/gdash/gdash.conf',
-      notify => Service['httpd'];
-  }
-}
---
+++ manifests/vhost.pp
@@ -0,0 +1,12 @@
+class gdash::vhost {
+
+  file {
+    "/etc/httpd/conf.d/gdash.conf":
+      ensure  => 'file',
+      group   => '0',
+      mode    => '0644',
+      owner   => '0',
+      source => 'puppet:///modules/gdash/gdash.conf',
+      notify => Service['httpd'];
+  }
+}
